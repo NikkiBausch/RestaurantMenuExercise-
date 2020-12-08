9a197578ecbb4efe8994270f29f7d0dd4f586822
NikkiBeeLili <arcanahereticae13@gmail.com>
2020-12-08 16:35:50 -0600

    Added Constructor

--- Restaurant/src/Dinners.java
+++ Restaurant/src/Dinners.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dinners {
-    ArrayList<String> dinnerChoice1 = new ArrayList<>();
-        dinnerChoice1.add("Breaded Mushrooms");
-        dinnerChoice1.add("Baked Potato");
-        dinnerChoice1.add("Tartar Sauce");
+       public Dinners(){
+               ArrayList<String> dinnerChoice1 = new ArrayList<>();
+               dinnerChoice1.add("Breaded Mushrooms");
+               dinnerChoice1.add("Baked Potato");
+               dinnerChoice1.add("Tartar Sauce");
+
+               ArrayList<String> dinnerChoice2 = new ArrayList<>();
+               dinnerChoice2.add("Orange Tofu");
+               dinnerChoice2.add("Rice");
+               dinnerChoice2.add("avocado veggie sushi");
+
+               ArrayList<String> dinnerChoice3 = new ArrayList<>();
+               dinnerChoice3.add("vegan Chik'n nuggets");
+               dinnerChoice3.add("French Fries");
+               dinnerChoice3.add("vegan lemon mayo");
 
 
-    ArrayList<String> dinnerChoice2 = new ArrayList<>();
-        dinnerChoice2.add("Orange Tofu");
-        dinnerChoice2.add("Rice");
-        dinnerChoice2.add("avocado veggie sushi");
 
-    ArrayList<String> dinnerChoice3 = new ArrayList<>();
-            dinnerChoice3.add("vegan Chik'n nuggets");
-            dinnerChoice3.add("French Fries");
-            dinnerChoice3.add("vegan lemon mayo");
+
+
+
+
+
+    }
 }
